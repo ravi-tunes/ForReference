cb216d1f9a9bee21063536a625bced7615ceb116
ravi-tunes <ravi.itune.s@gmail.com>
2025-06-02 14:08:49 +0530

    Delete Keytab directory

--- Keytab/retry.java
+++
@@ -1,7 +0,0 @@
-@Retryable(
-    value = { LoginException.class },
-    maxAttempts = 3,
-    backoff = @Backoff(delay = 2000)
-public Subject loginWithKeytab() throws LoginException {
-    // login logic
-}
